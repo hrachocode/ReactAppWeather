db1357218dca98c764d1c5e3b8ce9a4287457d51
hrachocode <hracho94@yahoo.com>
2017-09-29 16:13:21 +0400

    Http forwarding settings changed

--- server.js
+++ server.js
@@ -2,16 +2,21 @@ var express = require('express');
 
 var app = express();
 const PORT = process.env.PORT || 3000;
-
-app.use(express.static('public'));
-
+var http = 'http';
 app.use(function(req, res, next) {
-   if (req.headers['x-forwarded-proto'] === 'https') {
+    if (req.headers['referer'].includes('https')) {
       res.redirect('http://' + req.hostname + req.url);
     } else {
       next();
     }
+   // if (req.headers['x-forwarded-proto'] === 'https') {
+   //    res.redirect('http://' + req.hostname + req.url);
+   //  } else {
+   //    next();
+   //  }
 });
+
+app.use(express.static('public'));
 
 app.listen(PORT, function(){
 	console.log("Starting some dope shit " + PORT);
