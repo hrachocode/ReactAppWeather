984b1e40adf961d88e6482441491897a73ddee00
hrachocode <hracho94@yahoo.com>
2017-09-29 15:20:59 +0400

    Change to Default Port, added Http redirection Cheking

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 var express = require('express');
 
 var app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
-app.listen(3000, function(){
-	console.log("Starting some dope shit");
+app.use(function(req, res, next) {
+	if(req.headers['x-forwarded-proto'] === 'http'){
+	  	next();
+	}else{
+		res.redirect('http://' + req.hostname + req.url);
+	}
+})
+
+app.listen(PORT, function(){
+	console.log("Starting some dope shit " + PORT);
 });
