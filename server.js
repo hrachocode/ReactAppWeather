c8c8502dd9c8534728b22bbcdc661faaf61229d4
hrachocode <hracho94@yahoo.com>
2017-09-29 15:40:27 +0400

    Handled Http error

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static('public'));
 
 app.use(function(req, res, next) {
-	if(req.headers['x-forwarded-proto'] === 'http'){
-	  	next();
-	}else{
-		res.redirect('https://' + req.hostname + req.url);
-	}
-})
+   if (req.headers['x-forwarded-proto'] === 'https') {
+      res.redirect('http://' + req.hostname + req.url);
+    } else {
+      next();
+    }
+});
 
 app.listen(PORT, function(){
 	console.log("Starting some dope shit " + PORT);
